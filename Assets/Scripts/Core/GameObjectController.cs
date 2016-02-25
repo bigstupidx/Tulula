1d3aa37df9a6bb7bbb48107c907624aac42b2cd0
motluah.vladimir <motluah.vladimir@kevurugames.com>
2016-02-25 23:23:04 +0200

    adding files

---
+++ Assets/Scripts/Core/GameObjectController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+public delegate void GameObjectEvent(GameObject obj);
+
+public class GameObjectController : MonoBehaviour
+{
+    [SerializeField]
+    private float _alpha = 1.0f;
+    [SerializeField]
+    private bool _touchEnabled = false;
+    [SerializeField]
+    private bool _ignoreAlpha = false;
+
+    public event GameObjectEvent onTouchBegan;
+    public event GameObjectEvent onTouchEnded;
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        UpdateTransparency();
+    }
+
+    void UpdateTransparency()
+    {
+        var renderer = gameObject.GetComponent<SpriteRenderer>();
+
+        if (renderer != null)
+        {
+            float alpha = _alpha;
+            var parent = gameObject.transform.parent;
+
+            if (parent != null)
+            {
+                var parentRenderer = parent.GetComponent<SpriteRenderer>();
+
+                if (parentRenderer != null)
+                {
+                    alpha *= parentRenderer.color.a;
+                }
+            }
+
+            Color color = renderer.color;
+            color.a = alpha;
+
+            renderer.color = color;
+        }
+    }
+
+    public float alpha
+    {
+        get{
+            return _alpha;
+        }
+
+        set{
+            _alpha = value;
+            UpdateTransparency();
+        }
+    }
+
+    public Vector3 center
+    {
+        get {
+            var collider = gameObject.GetComponent<Collider2D>();
+            var center = gameObject.transform.position;
+
+            if (collider)
+            {
+                center += collider.bounds.size / 2;
+            }
+
+            return center;
+        }
+    }
+
+    public bool isTouchEnabled
+    {
+        get { return _touchEnabled; }
+        set { _touchEnabled = value; }
+    }
+
+    public bool isIgnoreAlpha
+    {
+        get { return _ignoreAlpha; }
+        set { _ignoreAlpha = value; }
+    }
+
+    public bool isAviableForTouch
+    {
+        get { return isTouchEnabled && (alpha > 0 || isIgnoreAlpha); }
+    }
+
+    public void OnTouchBegan()
+    {
+        if(onTouchBegan != null)
+        {
+            onTouchBegan(gameObject);
+        }
+    }
+
+    public void OnTouchEnded()
+    {
+        if(onTouchEnded != null)
+        {
+            onTouchEnded(gameObject);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Core/SceneController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+    GameObject _picked = null;
+    GameObject _touched = null;
+
+    List<GameObject> _inventory = new List<GameObject>();
+
+	void Start ()
+    {
+        var tc = gameObject.AddComponent<TouchController>();
+
+        tc.onTouchBegan += OnTouchBegan;
+        tc.onTouchMoved += OnTouchMoved;
+        tc.onTouchEnded += OnTouchEnded;
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void OnTouchBegan(TouchEvent touch)
+    {
+        _touched = GetObjectAtTouch(touch);
+
+        if(_touched != null)
+        {
+            var controller = _touched.GetComponent<GameObjectController>();
+            if(controller != null)
+            {
+                var invent = _touched.GetComponent<InventController>();
+
+                controller.OnTouchBegan();
+
+                if(invent)
+                {
+                    invent.Invent();
+                }
+            }
+        }
+    }
+
+    void OnTouchMoved(TouchEvent touch)
+    {
+
+    }
+
+    void OnTouchEnded(TouchEvent touch)
+    {
+        if (_touched != null)
+        {
+            var obj = GetObjectAtTouch(touch);
+
+            if(obj == _touched)
+            {
+                var controller = _touched.GetComponent<GameObjectController>();
+                if (controller != null)
+                {
+                    controller.OnTouchEnded();
+                }
+            }
+
+            _touched = null;
+        }
+    }
+
+    GameObject GetObjectAtTouch(TouchEvent touch)
+    {
+        var location = Camera.main.ScreenToWorldPoint(touch.location);
+        var colliders = Physics2D.OverlapPointAll(new Vector2(location.x, location.y));
+
+        foreach (var collider in colliders)
+        {
+            var obj = collider.transform.gameObject;
+            var controller = obj.GetComponent<GameObjectController>();
+
+            if (controller)
+            {
+                if (controller.isAviableForTouch)
+                {
+                    return obj;
+                }
+            }
+        }
+
+        return null;
+    }
+}
