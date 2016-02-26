c61761ebc985094911c3f03b63c7f771b5b61329
Motluah.Vladimir <motluah.vladimir@kevurugames.com>
2016-02-26 22:08:01 +0200

    inventing splines

--- Assets/Scripts/Core/InventController.cs
+++ Assets/Scripts/Core/InventController.cs
@@ -15,6 +15,11 @@ class InventController : MonoBehaviour
     InventState _state;
     BezierSpline _spline;
 
+    static float kSpeed = 1.0f;
+
+    float _time = 0;
+    float _duration = 0;
+
     public InventState state
     {
         get { return _state; }
@@ -22,23 +27,37 @@ class InventController : MonoBehaviour
 
     public void Update()
     {
+        if(_spline)
+        {
+            _time += Time.deltaTime;
 
+            Debug.Log(_time);
+            transform.localPosition = _spline.GetPoint(_time / 5.0f);
+
+            if(_time >= 5.0f)
+            {
+                _spline = null;
+            }
+        }
     }
 
     public void Invent(Vector3 targetPosition)
     {
         var go = new GameObject();
 
-        float f = targetPosition.x < gameObject.transform.position.x ? 1.0f : -1.0f;
+        float f = targetPosition.x < transform.position.x ? 1.0f : -1.0f;
 
-        _spline = go.AddComponent<BezierSpline>();
+        _spline = gameObject.AddComponent<BezierSpline>();
         _spline.Reset();
 
         _spline.SetControlPoint(0, targetPosition);
-        _spline.SetControlPoint(3, gameObject.transform.position);
+        _spline.SetControlPoint(3, transform.position);
         _spline.SetControlPoint(1, targetPosition + Quaternion.Euler(0, 0, 45 * f) * new Vector3(0, 2));
-        _spline.SetControlPoint(2, gameObject.transform.position + Quaternion.Euler(0, 0, 0) * new Vector3(0, 2));
-  
+        _spline.SetControlPoint(2, transform.position + Quaternion.Euler(0, 0, 0) * new Vector3(0, 2));
+          
         _state = InventState.Invent;
+
+        _time = 0;
+        _duration = Vector3.Distance(targetPosition, transform.position) / kSpeed;
     }
 }
